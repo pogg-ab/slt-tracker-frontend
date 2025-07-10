738d621d42d086e1a15dc8b08d5d9ca26467a67a
Abel Legesse <abellegesse29@gmail.com>
2025-07-10 10:52:53 -0700

    feat: Configure API client for production deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'https://api.tracker.skylinkict.com/api',
+    baseURL: `${process.env.REACT_APP_API_URL}/api`,
     headers: { 'Content-Type': 'application/json' },
 });
 
